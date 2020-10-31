9a2d0c5c8fabcb2611ae4a19e48fa41d9a7af57e
jiarong <jiarong.xu@ucalgary.ca>
2020-10-30 20:10:06 -0700

    formatting and add method exception

--- Inspector.java
+++ Inspector.java
@@ -19,112 +19,131 @@ public class Inspector {
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
 
         String name = c.getName(); //The name of the declaring class
-        print("CLASS" + "\n" + "Class: " + name, depth);
+        indent("CLASS", depth);
+        indent("Class: " + name, depth);
 
-        getSuperClassNames(c, obj, recursive, depth); //The name of the immediate super-class
-        getInterfacesNames(c, obj, recursive, depth);
-        getConstructors(c, depth);
-        getMethods(c, depth);
-        getFields(c, depth);
+        getSuperClassNames(c, obj, recursive, depth + 1); //The name of the immediate super-class
+        getInterfacesNames(c, obj, recursive, depth + 1);
+        getConstructors(c, depth + 1);
+        getMethods(c, depth + 1);
+        getFields(c, depth + 1);
 
         // List<String> actualFieldNames = getFieldNames(fields); // The name of the filed
-        // print("ActualFieldNames: " + actualFieldNames);
+        // indent("ActualFieldNames: " + actualFieldNames);
     }
 
     public void getFields(Class c, int depth) {
-      depth += 1;
+      int depthNum = depth + 1;
       Field[] fields = c.getDeclaredFields();
       String modifier = Integer.toString(c.getModifiers());
-      print("modifier: " + modifier, depth);
+      indent("modifier: " + modifier, depth);
 
       if (fields.length > 0) {
-          for (Field field : fields) {
-            String fieldName = field.getName();
-            Object fieldType = field.getType();
-            print("FieldName: " + fieldName, depth);
-            print("FieldType: " + fieldType.toString(), depth);
-          }
+        for (Field field : fields) {
+          String fieldName = field.getName();
+          Object fieldType = field.getType();
+          indent("FieldName: " + fieldName, depth);
+          indent("FieldType: " + fieldType.toString(), depth);
+        }
+      }else {
+        indent("FieldName -> NONE", depth);
       }
     }
 
     public void getConstructors(Class c, int depth) {
-      depth += 1;
+      int depthNum = depth + 1;
       Constructor[] constructors = c.getConstructors();
       String modifier = Integer.toString(c.getModifiers());
-      print("modifier: " + modifier, depth);
       if (constructors.length > 0) {
-          for (Constructor constructor : constructors) {
-            print("CONSTRUCTOR" + "\n" + "Constructors Name: " + constructor.getName().toString(), depth);
-            //print("parameterTypes: " + parameterTypes.toString());
+        indent("CONSTRUCTORS" + " ( " + "   " + " ) ", depth);
+        indent("Constructors->", depth);
+        for (Constructor constructor : constructors) {
+          indent("Constructors Name: " + constructor.getName().toString(), depth);
+          indent("modifier: " + modifier, depthNum);
 
-            Class[] parameterTypes = constructor.getParameterTypes();
-            if (parameterTypes.length > 0) {
-                for (Class parameterType : parameterTypes) {
-                    print("Constructor Parameter Types: " + parameterType.getName().toString(), depth);
-                }
-            }
+          Class[] parameterTypes = constructor.getParameterTypes();
+          if (parameterTypes.length > 0) {
+              for (Class parameterType : parameterTypes) {
+                  indent("Constructor Parameter Types: " + parameterType.getName().toString(), depthNum);
+              }
           }
+        }
+      }else {
+        indent("Constructor -> NONE", depth);
       }
     }
 
     public void getMethods(Class c, int depth) {
-      depth += 1;
+      int depthNum = depth + 1;
       Method[] methods = c.getMethods();
       String modifier = Integer.toString(c.getModifiers());
-      print("modifier: " + modifier, depth);
+      indent("Modifier: " + modifier, depth);
       if (methods.length > 0) {
+          indent("METHODS" + " ( " + "   " + " ) ", depth);
+          indent("Methods->", depth);
           for (Method method : methods) {
-            //Class[] parameterTypes = method.getParameterTypes();
-            Class returnType = method.getReturnType();
-            print("MethodsName: " + method.getName().toString(), depth);
-            //print("parameterTypes: " + parameterTypes.toString());
-            print("ReturnType: " + returnType.toString(), depth);
-
+            Class[] exceptions = method.getExceptionTypes();
+            if (exceptions.length > 0) {
+                for (Class exception: exceptions) {
+                    indent("Exceptions: " + exception.getName().toString(), depthNum);
+                }
+            }
             Class[] parameterTypes = method.getParameterTypes();
             if (parameterTypes.length > 0) {
                 for (Class parameterType : parameterTypes) {
-                    print("Method Parameter Types: " + parameterType.getName().toString(), depth);
+                    indent("Method Parameter Types: " + parameterType.getName().toString(), depthNum);
                 }
             }
+            Class returnType = method.getReturnType();
+            indent("MethodsName: " + method.getName().toString(), depth);
+            indent("ReturnType: " + returnType.toString(), depthNum);
           }
+      }else {
+        indent("Method -> NONE", depth);
       }
+
     }
 
-    private void getSuperClassNames(Class c, Object obj, boolean recursive, int depth) {
-      depth += 1;
+    public void getSuperClassNames(Class c, Object obj, boolean recursive, int depth) {
+      int depthNum = depth + 1;
       Class superClass = c.getSuperclass();
 
       if (superClass != null) {
-          print("\t" + "SUPERCLASS --> Recursively Insepct" +"\n" + "\t" + "SuperClass: " + superClass.getName().toString(), depth);
-          inspectClass(superClass, obj, recursive, depth);
+          indent("SUPERCLASS --> Recursively Insepct", depthNum);
+          indent("SuperClass: " + superClass.getName().toString(), depthNum);
+          inspectClass(superClass, obj, recursive, depthNum);
+      }else {
+        indent("SuperClass -> NONE", depth);
       }
     }
 
-    private void getInterfacesNames(Class c, Object obj, boolean recursive, int depth) {
-      depth += 1;
+    public void getInterfacesNames(Class c, Object obj, boolean recursive, int depth) {
+      int depthNum = depth + 1;
       Class[] interfaces = c.getInterfaces();
 
       if (interfaces != null) {
         for(Class interfaceName : interfaces) {
-          print("INTERFACE" + "\n" + "Interfaces: " + interfaceName.toString(), depth);
-          inspectClass(interfaceName, obj, recursive, depth);
+          indent("INTERFACE", depth);
+          indent("Interfaces: " + interfaceName.toString(), depth);
+          inspectClass(interfaceName, obj, recursive, depthNum);
         }
+      }else {
+        indent("Interface -> NONE", depth);
       }
     }
 
-    private static List<String> getFieldNames(Field[] fields) {
+    public static List<String> getFieldNames(Field[] fields) {
       List<String> fieldNames = new ArrayList<>();
       for (Field field : fields)
         fieldNames.add(field.getName());
       return fieldNames;
     }
 
-    private void print(String string, int depth) {
+    private void indent(String string, int depth) {
       for (int i = 0; i < depth; i++)
           System.out.print("\t");
       System.out.println(string);
     }
-
 }
 
 
@@ -143,5 +162,5 @@ public class Inspector {
 // b. The type
 // c. The modifiers
 // d. The current value
-// i. If the field is an object reference, and recursive is set to false, then simply print out the “reference value” directly (this will be the name of the object’s class plus the object’s “identity hash code” ex. java.lang.Object@7d4991ad).
+// i. If the field is an object reference, and recursive is set to false, then simply indent out the “reference value” directly (this will be the name of the object’s class plus the object’s “identity hash code” ex. java.lang.Object@7d4991ad).
 // ii. If the field is an object reference, and recursive is set to true, then immediately recurse on the object.
